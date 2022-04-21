d7a2e66456179fb569042f9c97ba512fa54067bb
Thomas Boittin <tboittin@gmail.com>
2022-04-21 18:20:47 +0200

    gallery image position

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -2,12 +2,22 @@ import { FC } from "react"
 import styled from "styled-components"
 
 const Container = styled.div`
+  display: flex;
   background-color: orange;
-  height: 20vh;
+  align-items: center;
+  img {
+    height: 300px;
+    width: 100%;
+    object-fit: cover;
+  }
 `
 
 const Gallery: FC = () => {
-  return <Container>Gallery</Container>
+  return (
+    <Container>
+      <img src='./image-product-1.jpg' alt='Sneakers' />
+    </Container>
+  )
 }
 
 export default Gallery
