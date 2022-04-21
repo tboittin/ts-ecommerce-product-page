014d8b72a10e41e7a452c38926d4562addd2600d
Thomas Boittin <tboittin@gmail.com>
2022-04-21 18:37:40 +0200

    description

--- src/components/Description/index.tsx
+++ src/components/Description/index.tsx
@@ -1,9 +1,37 @@
-import { FC } from "react";
+import { FC } from "react"
+import styled from "styled-components"
 
-const Description:FC = () => {
-    return (
-        <p>Description</p>
-    )
+const Container = styled.div`
+  margin: 25px 20px;
+  h3 {
+    text-transform: uppercase;
+    color: #e3975a;
+    font-size: 14px;
+    letter-spacing: 1px;
+  }
+  h2 {
+    font-size: 30px;
+    line-height: 36px;
+    margin: 20px 0;
+  }
+  p {
+      color: #777A81;
+      line-height: 26px;
+  }
+`
+
+const Description: FC = () => {
+  return (
+    <Container>
+      <h3>Sneaker Company</h3>
+      <h2>Fall Limited Edition Sneakers</h2>
+      <p>
+        These low-profile sneakers are your perfect casual wear companion.
+        Featuring a durable rubber outer sole, they'll withstand everything the
+        weather can offer.
+      </p>
+    </Container>
+  )
 }
 
 export default Description
