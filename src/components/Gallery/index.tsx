eb98471c0d53557c147026f3893a8e6dfd9db9fa
Thomas Boittin <tboittin@gmail.com>
2022-04-21 18:09:29 +0200

    navbar

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -1,9 +1,13 @@
-import { FC } from "react";
+import { FC } from "react"
+import styled from "styled-components"
+
+const Container = styled.div`
+  background-color: orange;
+  height: 20vh;
+`
 
 const Gallery: FC = () => {
-    return (
-        <p>Gallery</p>
-    )
+  return <Container>Gallery</Container>
 }
 
 export default Gallery
--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,16 +1,51 @@
 import { FC } from "react"
-import  { ReactComponent as IconMenu} from '../../images/icon-menu.svg'
-import  { ReactComponent as IconCart} from '../../images/icon-cart.svg'
+import { ReactComponent as IconMenu } from "../../images/icon-menu.svg"
+import { ReactComponent as IconCart } from "../../images/icon-cart.svg"
+import styled from "styled-components"
+
+const Container = styled.div`
+  height: 55px;
+  margin: 15px 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  .left-navbar {
+    display: flex;
+    align-items: center;
+    h1 {
+      font-family: "Poppins", sans-serif;
+      margin: 0 12px;
+      letter-spacing: -3%;
+      font-size: 30px;
+      line-height: 21px;
+    }
+  }
+`
+
+const IconContainer = styled.div`
+  display: flex;
+  align-items: center;
+
+  img {
+    height: 25px;
+    margin-left: 18px;
+  }
+`
 
 const NavBar: FC = () => {
-    return (
-        <div>
-            <IconMenu />
-            <div>sneakers</div>
-            <IconCart />
-            <img src="image-avatar.png" alt="avatar" />
-        </div>
-    )
+  return (
+    <Container>
+      <div className='left-navbar'>
+        <IconMenu />
+        <h1>sneakers</h1>
+      </div>
+      <IconContainer>
+        <IconCart />
+        <img src='image-avatar.png' alt='avatar' />
+      </IconContainer>
+    </Container>
+  )
 }
 
 export default NavBar
